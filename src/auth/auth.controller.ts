d5824f0f7f11cadd8550a86aaf387540578a9c22
Malik Samansya <maliksamansya206@gmail.com>
2024-04-18 21:09:04 +0700

    feat: updaate dep

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,19 @@
 // auth/auth.controller.ts
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+// import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './guards/local-auth.guard';
+// import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
+  // @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return this.authService.createToken(req.user.id, req.user.username);
+    console.log(req);
+    const { id, username } = req.body;
+    // return this.authService.createToken(req.user.id, req.user.username);
+    return this.authService.createToken(id, username);
   }
 }
